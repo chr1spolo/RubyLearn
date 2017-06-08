7b5c8bb21e97c1c0a69045f58635b35eecd08004
chr1spolo <polidavis28@gmail.com>
2017-06-08 18:25:52 -0500

    convertidor function

--- conversion.rb
+++ conversion.rb
@@ -2,9 +2,18 @@
 # moneda local
 
 
-#variable que obtendrá la cantidad de dolares a convertir
-dolares = 300
-#variable que obtendrá el porcentaje de cambio
-tasa_de_cambio = 17.77
 
-puts dolares * tasa_de_cambio
+def dolar_conversion( dolar_to_convert , percentaje )
+
+#variable que obtendrá la cantidad de dolares a convertir
+dolares = dolar_to_convert
+#variable que obtendrá el porcentaje de cambio
+tasa_de_cambio = percentaje
+
+return dolares * tasa_de_cambio
+    
+end
+
+
+puts dolar_conversion(800, 17.77)
+puts dolar_conversion(1200, 17.77)
