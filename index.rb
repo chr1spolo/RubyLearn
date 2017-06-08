3275203d5e59fb9a5a505994f8dbadba2299edbb
chr1spolo <polidavis28@gmail.com>
2017-06-08 17:37:17 -0500

    first commit

---
+++ index.rb
@@ -0,0 +1,10 @@
+# la palabra reservada puts se utiliza para emitir un mensaje
+
+puts "Hello World"
+
+# las variables no se definen, y los metodos solo se llaman con su nombre si 
+# no se le asignan parametros encerrados entre parentesis
+
+nombre_del_curso = "lear ruby"
+
+puts nombre_del_curso
